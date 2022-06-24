c35c2fa35ec9fd2ad8993c5d785cfa31dbf677b6
juttayaya <jirawat.uttayaya@rocketinsights.com>
2022-06-24 11:31:26 -0400

    added tf changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "ju-github" {
-  name         = "ju-github5"
+resource "aws_dynamodb_table" "terraform_github_actions_example" {
+  name         = "terraform-github-actions-example"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "KeyId"
 
